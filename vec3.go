eace1204aa8d0dd7ae505c1a2f76f42a16ef14b6
reconditematter <recondite.matter@gmail.com>
2019-11-27 11:08:02 -0500

    Implement basic operations on 3-d vectors.

---
+++ vec3.go
@@ -0,0 +1,57 @@
+package mym
+
+import (
+	"math"
+)
+
+// Vneg3 -- returns -u.
+func Vneg3(u [3]float64) [3]float64 {
+	return [3]float64{-u[0], -u[1], -u[2]}
+}
+
+// Vadd3 -- returns u+v.
+func Vadd3(u, v [3]float64) [3]float64 {
+	return [3]float64{u[0] + v[0], u[1] + v[1], u[2] + v[2]}
+}
+
+// Vsub3 -- returns u-v.
+func Vsub3(u, v [3]float64) [3]float64 {
+	return [3]float64{u[0] - v[0], u[1] - v[1], u[2] - v[2]}
+}
+
+// Vmul3 -- returns s·u.
+func Vmul3(u [3]float64, s float64) [3]float64 {
+	return [3]float64{s * u[0], s * u[1], s * u[2]}
+}
+
+// Vdiv3 -- returns u/s.
+func Vdiv3(u [3]float64, s float64) [3]float64 {
+	return [3]float64{u[0] / s, u[1] / s, u[2] / s}
+}
+
+// Vdot3 -- returns u·v (scalar, or dot product).
+func Vdot3(u, v [3]float64) float64 {
+	return u[0]*v[0] + u[1]*v[1] + u[2]*v[2]
+}
+
+// Vcrs3 -- returns u⨯v (vector, or cross product).
+func Vcrs3(u, v [3]float64) [3]float64 {
+	x := u[1]*v[2] - u[2]*v[1]
+	y := u[2]*v[0] - u[0]*v[2]
+	z := u[0]*v[1] - u[1]*v[0]
+	return [3]float64{x, y, z}
+}
+
+// Vabs3 -- returns |u| (L2 norm).
+func Vabs3(u [3]float64) float64 {
+	return math.Hypot(math.Hypot(u[0], u[1]), u[2])
+}
+
+// Vhat3 -- returns u/|u|. Returns a zero vector when |u|<ε.
+func Vhat3(u [3]float64) [3]float64 {
+	s := math.Hypot(math.Hypot(u[0], u[1]), u[2])
+	if s >= Epsilon {
+		return [3]float64{u[0] / s, u[1] / s, u[2] / s}
+	}
+	return [3]float64{0, 0, 0}
+}
