ea30843b8f6ea23ff86da1ee83af6755610379cc
reconditematter <recondite.matter@gmail.com>
2020-05-06 15:43:04 -0400

    Implement the complete elliptic integrals of the first and second kinds. Implement a test for the above integrals using the Legendre's relation.

---
+++ compellint_test.go
@@ -0,0 +1,23 @@
+package mym
+
+import (
+	"math"
+	"testing"
+)
+
+// TestComplete12 checks if the Legendre's relation for m=0.00001(0.00001)0.99999
+// is satisfied with an accuracy given by `tol`.
+func TestComplete12(t *testing.T) {
+	const tol = 1.0e-14
+
+	for m := 1; m < 100000; m++ {
+		mf := float64(m) / 100000
+		mp := 1 - mf
+		K, E := Complete12(mf)
+		Kp, Ep := Complete12(mp)
+		legendre := math.Abs(E*Kp + Ep*K - K*Kp - math.Pi/2)
+		if legendre > tol {
+			t.Fatalf("legendre > tol: m=%v, legendre=%v, tol=%v", mf, legendre, tol)
+		}
+	}
+}
