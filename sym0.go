c730b030145ff4b45fe42ec3e4328fca5060a356
Leonid Kneller <reconditematter@gmail.com>
2020-08-11 09:53:19 -0400

    Implement `Sym0`.

---
+++ sym0.go
@@ -0,0 +1,49 @@
+package mym
+
+// Sym0 -- represents a symmetric matrix with zero diagonal.
+type Sym0 struct {
+	n int
+	a []float64
+}
+
+// NewSym0 -- returns an n-by-n symmetric matrix with all zero elements.
+func NewSym0(n int) Sym0 {
+	if n < 1 {
+		panic("mym.NewSym0: n < 1")
+	}
+	return Sym0{n, make([]float64, n*(n-1)/2)}
+}
+
+// N -- returns the matrix size.
+func (a Sym0) N() int {
+	return a.n
+}
+
+// Get -- returns a[i,j].
+func (a Sym0) Get(i, j int) float64 {
+	if !(0 <= i && i < a.n && 0 <= j && j < a.n) {
+		panic("mym.Sym0.Get: index")
+	}
+	if i < j {
+		return a.a[i*a.n+j]
+	}
+	if i > j {
+		return a.a[j*a.n+i]
+	}
+	return 0
+}
+
+// Set -- assigns a[i,j] = x.
+func (a Sym0) Set(i, j int, x float64) {
+	if !(0 <= i && i < a.n && 0 <= j && j < a.n) {
+		panic("mym.Sym0.Set: index")
+	}
+	if i < j {
+		a.a[i*a.n+j] = x
+		return
+	}
+	if i > j {
+		a.a[j*a.n+i] = x
+		return
+	}
+}
