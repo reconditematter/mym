93cb6111845d91ef85786b6f8778d89f9e1bdd23
Leonid Kneller <recondite.matter@gmail.com>
2021-02-05 16:05:47 -0500

    Update (c) year.

--- mt19937i_test.go
+++ mt19937i_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2019-2020 Leonid Kneller. All rights reserved.
+// Copyright (c) 2019-2021 Leonid Kneller. All rights reserved.
 // Licensed under the MIT license.
 // See the LICENSE file for full license information.
 
